52dbcc712c8701b534d421e9796346f59d276d02
HannahKettor <warsonga95@gmail.com>
2022-12-02 16:23:34 +0000

    done with contactus component

--- skolis-app/src/App.js
+++ skolis-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
     <>
       <Navbar />
+      <ContactUs />
 
       <div class="py-6">
         <main>
