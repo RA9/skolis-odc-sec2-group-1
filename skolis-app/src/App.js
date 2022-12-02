427dae143c080d00d2fe275104f94f206f3e73ed
Menson B Yormie <mensonbyormie@gmail.com>
2022-12-02 15:53:25 +0000

    Done with Login component

--- skolis-app/src/App.js
+++ skolis-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <>
       <Navbar />
+      <Login/>
 
       <div class="py-6">
         <main>
