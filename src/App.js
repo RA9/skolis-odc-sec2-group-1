f206c5f4db970edd2874faaf6e980d11895cb512
Alphabarrie98 <barriealpha24@gmail.com>
2022-12-05 15:17:17 +0000

    Register

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import Register from './components/Register';
+
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Register />
+      
       
     </div>
     
