bd975c1f6387d4c01bae79aeee1f31b1b03d0ff3
Menson B Yormie <mensonbyormie@gmail.com>
2022-12-05 14:45:22 +0000

    Yormie Login Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
       <Navbar />
+      {/* <Login /> */}
     </div>
     
   );
